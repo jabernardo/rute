111c8c5bd1107762f9ec111e31612bb78e94bec0
John Aldrich Bernardo <4ldrich@protonmail.com>
2020-04-10 07:52:47 -0400

    update logger

--- middlewares/logger.ts
+++ middlewares/logger.ts
@@ -3,6 +3,16 @@ import { Request } from "../request.ts";
 import { Response } from "../response.ts";
 
 export const Logger: Middleware = async (req: Request, res: Response, n: Next) => {
-  console.log(`[${req.method}] ${req.url.href}`);
+  const { remoteAddr, localAddr} = req.connection;
+
+  const localPort = "port"  in localAddr ? localAddr.port : "";
+  const localTransport = "transport" in localAddr ? localAddr.transport : "";
+  const localHostName = "hostname" in localAddr ? localAddr.hostname : "";
+
+  const remoteHostName = "hostname" in remoteAddr ? remoteAddr.hostname : "";
+  const remotePort = "port"  in remoteAddr ? remoteAddr.port : "";
+  const remoteTransport = "transport" in remoteAddr ? remoteAddr.transport : "";
+
+  console.log(`@ ${localHostName}:${localPort}/${localTransport} ( from ${remoteHostName}:${remotePort}/${remoteTransport} - ${req.method} - ${req.url.href} )`);
   await n();
 }
