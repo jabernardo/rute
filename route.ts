e1d897f3c41ad83b9cd8ce101ef807a8b1a1e783
John Aldrich Bernardo <4ldrich@protonmail.com>
2020-04-10 22:42:17 -0400

    added comments

--- route.ts
+++ route.ts
@@ -7,13 +7,33 @@ export interface Routes {
   [path: string]: Route
 }
 
+/**
+ * Route handler function
+ *
+ * @param  request   Request
+ * @param  response  Response
+ * @return void
+ *
+ */
 export type RouteHandler = (request: Request, response: Response) => void;
 
+/**
+ * Route instance
+ *
+ */
 export class Route extends MiddlewareContainer {
   private _path: string;
   private _method: string | Array<string>;
   private _handler: RouteHandler;
 
+  /**
+   * Create an instance of Route
+   *
+   * @param   path     string                  Route path
+   * @param   method   string | Array<string>  HTTP Method
+   * @param   hander   RouteHandler
+   *
+   */
   constructor(path: string, method: string | Array<string>, handler: RouteHandler) {
     super();
 
@@ -22,18 +42,52 @@ export class Route extends MiddlewareContainer {
     this._handler = handler;
   }
 
-  async use(fn: Middleware) {
-    await super.use(fn);
+  /**
+   * Add middleware
+   *
+   * @example
+   *
+   *  const app = new Rute();
+   *  app.use(async (req: Request, res: Response, n: Next) => {
+   *    //...todo
+   *  });
+   *
+   * @param   fn   Middleware   Middleware
+   * @return  void
+   *
+   */
+  use(fn: Middleware) {
+    super.use(fn);
   }
 
+  /**
+   * Get route path
+   *
+   * @return   string
+   *
+   */
   get path(): string {
     return this._path;
   }
 
+  /**
+   * Get route HTTP method
+   *
+   * @return   string
+   *
+   */
   get method(): string | Array<string> {
     return this._method;
   }
 
+  /**
+   * Execute route
+   *
+   * @param   httpRequest   Request
+   * @param   httpResponse  Reponse
+   * @return  void
+   *
+   */
   async execute(httpRequest: Request, httpResponse: Response) {
     await this.go(httpRequest, httpResponse, async () => {
       if (this._method.indexOf(httpRequest.method) > -1)  {
