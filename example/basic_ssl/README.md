1de8ca8b1a711872b7812f7767be163d2dcb8251
John Aldrich Bernardo <4ldrich@protonmail.com>
2020-05-07 11:24:25 +0800

    add an ssl example using mkcert

---
+++ example/basic_ssl/README.md
@@ -0,0 +1,15 @@
+# Running this example
+
+## Requirements
+- [mkcert](https://github.com/FiloSottile/mkcert)
+
+
+## Go!
+
+```sh
+make
+```
+
+## Links
+- [localhost:8080](http://localhost:8080)
+- [localhost:8443](https://localhost:8443)
---
+++ example/basic_ssl/app.ts
@@ -0,0 +1,62 @@
+import { Server, Request, Response, Middleware, Next, HTTP } from "../../mod.ts";
+
+const app: Server = new Server();
+
+/**
+ * Static file server
+ *   http://localhost:8000/public/style.css
+ */
+app.static("./public");
+
+/**
+ * Middleware example
+ */
+app.use(async (req: Request, res: Response, n: Next) => {
+  console.log("[begin] middleware");
+  await n();
+  console.log("[end] middleware");
+});
+
+/**
+ * Index page
+ */
+app.all("/", async (req: Request, res: Response) => {
+  let data = await fetch("https://hacker-news.firebaseio.com/v0/item/2921983.json?print=pretty");
+  let json = await data.json();
+  res.cookie({ name: "test", value: "hello world!!!!" });
+  res.set(json);
+});
+
+/**
+ * A certain specific route middleware
+ *
+ */
+const specificRouteMiddleware = async (req: Request, res: Response, n: Next) => {
+  console.log("[begin] route middleware");
+  await n();
+  console.log("[end] route middleware");
+};
+
+/**
+ * Simple greeting
+ *   http://localhost:8000/hello-yourname
+ */
+app.get("/hello-{name}", (req: Request, res: Response) => {
+  res.set(`Hello, ${ req.param("name") } !`);
+}, specificRouteMiddleware);
+
+app.post("/hello-{name}", (req: Request, res: Response) => {
+  res.set(`Are you sure?, ${ req.param("name") } !`);
+}, specificRouteMiddleware);
+
+/**
+ * LET'S GO!!!
+ */
+app.listen({ port: 8080 });
+
+app.listen({
+  hostname: "localhost",
+  port: 8443,
+  certFile: "./ssl/cert.pem",
+  keyFile: "./ssl/key.pem"
+});
