f642a070724687274ebeb9413d0f0b9cd04550fa
John Aldrich Bernardo <4ldrich@protonmail.com>
2020-05-17 19:34:04 +0800

    lock std version to 0.51.0

--- deps.ts
+++ deps.ts
@@ -7,8 +7,7 @@ export {
   Response,
   HTTPOptions,
   HTTPSOptions,
-} from "https://deno.land/std/http/server.ts";
-
+} from "https://deno.land/std@0.51.0/http/server.ts";
 
 // Import: Deno HTTP Cookie Modules
 export {
@@ -17,17 +16,16 @@ export {
   setCookie,
   getCookies,
   delCookie,
-} from "https://deno.land/std/http/cookie.ts";
-
+} from "https://deno.land/std@0.51.0/http/cookie.ts";
 
 // Import: Deno Testing Modules
-export { assertEquals } from "https://deno.land/std/testing/asserts.ts";
+export { assertEquals } from "https://deno.land/std@0.51.0/testing/asserts.ts";
 
 // Import: Deno Path Modules
-export * as denoPath from "https://deno.land/std/path/mod.ts";
+export * as denoPath from "https://deno.land/std@0.51.0/path/mod.ts";
 
 // Import: Deno FS Modules
-export { existsSync } from "https://deno.land/std/fs/exists.ts";
+export { existsSync } from "https://deno.land/std@0.51.0/fs/exists.ts";
 
 // Import: Global from strcolors.ts
 export * from "https://raw.githubusercontent.com/jabernardo/strcolors.ts/0.1/mod.ts";
