84371c885ee99a648fe06280664cd0ed968843a5
John Aldrich Bernardo <4ldrich@protonmail.com>
2020-05-19 18:27:03 +0800

    preflight and removed route

--- middlewares/cors/mod.ts
+++ middlewares/cors/mod.ts
@@ -9,7 +9,6 @@
 import { Middleware, Next } from "../../middleware.ts";
 import { Request } from "../../request.ts";
 import { Response } from "../../response.ts";
-import { RouteHandler } from "../../route.ts";
 
 export type OriginList = string | string[] | RegExp | RegExp[];
 
@@ -21,17 +20,23 @@ export interface CorsOptions {
   exposeHeaders?: string[];
   credentials?: boolean;
   maxAge?: number;
+  preflightContinue?: boolean;
 }
 
 export const defaultOptions: CorsOptions = {
   origin: "*",
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   status: 200,
+  preflightContinue: false,
 };
 
 export type CorsCallback = (req: Request, res: Response) => void;
 
 function assertOrigin(origin: string, allowedOrigin: string | RegExp) {
+  if (typeof allowedOrigin === "string" && allowedOrigin === "*") {
+    allowedOrigin = new RegExp(".*");
+  }
+
   if (allowedOrigin instanceof RegExp) {
     return allowedOrigin.test(origin);
   }
@@ -59,19 +64,26 @@ export function configureCors(
   options?: CorsOptions | undefined,
 ) {
   const httpOptions: CorsOptions = options || defaultOptions;
+
   const origin: string = req.headers.get("Origin") || "";
-  const allowedOrigin: string = isOriginAllowed(origin, httpOptions.origin)
-    ? origin
-    : "false";
+  const allowedOrigin: string = isOriginAllowed(origin, httpOptions.origin) ? (origin || "*") : "false";
+
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", httpOptions.methods);
+
+    if (httpOptions.headers) {
+      const corsHeaders: string = httpOptions.headers.join(",");
+      res.header("Access-Control-Allow-Headers", corsHeaders);
+    }
+
+    if (httpOptions.maxAge) {
+      res.header("Access-Control-Max-Age", httpOptions.maxAge.toString());
+    }
+
+    res.header("Vary", "Origin");
+  }
 
   res.header("Access-Control-Allow-Origin", allowedOrigin);
-  res.header("Vary", "Origin");
-  res.header("Access-Control-Allow-Methods", httpOptions.methods);
-
-  if (httpOptions.headers) {
-    const corsHeaders: string = httpOptions.headers.join(",");
-    res.header("Access-Control-Allow-Headers", corsHeaders);
-  }
 
   if (httpOptions.exposeHeaders) {
     const corsExposeHeaders: string = httpOptions.exposeHeaders.join(",");
@@ -81,33 +93,11 @@ export function configureCors(
   if (httpOptions.credentials) {
     res.header("Access-Control-Allow-Credentials", "true");
   }
-
-  if (httpOptions.maxAge) {
-    res.header("Access-Control-Max-Age", httpOptions.maxAge.toString());
-  }
-}
-
-export function corsRoute(
-  options?: CorsOptions | undefined,
-  callback?: CorsCallback,
-): RouteHandler {
-  return (req: Request, res: Response) => {
-    configureCors(req, res, options);
-
-    res.header("Content-Length", "0");
-    res.status(options?.status || 204);
-
-    if (typeof callback !== "undefined") {
-      callback(req, res);
-    }
-  };
 }
 
 export function cors(options?: CorsOptions | undefined): Middleware {
   return async (req: Request, res: Response, n: Next) => {
     configureCors(req, res, options);
-
-    console.log(res.headers.get("Access-Control-Allow-Origin"));
 
     if (res.headers.get("Access-Control-Allow-Origin") === "false") {
       res.header("Content-Length", "0");
@@ -115,25 +105,11 @@ export function cors(options?: CorsOptions | undefined): Middleware {
       return;
     }
 
+    if (req.method === "OPTIONS" && options?.preflightContinue) {
+      await n();
+      return;
+    }
+
     await n();
   };
 }
-
-// const corsOptions = {
-//   origin: /(.*).mozilla.org/,
-//   methods: "POST",
-//   headers: ["apikey"],
-//   exposeHeaders: ["test"],
-//   maxAge: 10,
-//   credentials: true
-// };
-
-// const testCorseCheck: CorsCallback = (req: Request, res: Response) => {
-//   console.log("test");
-// }
-
-// app.options("/cors", corsRoute(corsOptions, testCorseCheck));
-
-// app.post("/cors", (req: Request, res: Response) => {
-//   res.set("CORS allowed page");
-// }, cors(corsOptions));
